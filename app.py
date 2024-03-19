36ddda8bd7d7825b0fe89d198152c05552008335
Bohdan-Orlyk <lollikwinner@gmail.com>
2024-03-19 19:56:07 +0100

    refactored + added sqlite db + created tables

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
+from flask_smorest import Api
 from flask import Flask
 
-app = Flask(__name__)
+from api.v1.store.store_router import blp as store_router
+from api.v1.store.items_router import blp as item_router
 
-stores = [
-    {
-        "name": "My Store",
-        "items": [
-            {
-                "name": "item_1",
-                "price": 15.99
-            }
-        ]
-    }
-]
+from db import db
 
 
-@app.get("/store")
-def get_stores():
-    return {"stores": stores}
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    api = Api(app)
+
+    with app.app_context():
+        db.create_all()
+
+    ROUTERS = [store_router, item_router]
+
+    [api.register_blueprint(router) for router in ROUTERS]
+
+    return app
